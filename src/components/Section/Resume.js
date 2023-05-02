68455e14553c7b927a3883b100cf04c21cfc6cdb
joshua-wade7 <joshua.nelson0124@gmail.com>
2023-05-01 23:39:55 -0600

    updated relative path for downloading resume.pdf

--- src/components/Section/Resume.js
+++ src/components/Section/Resume.js
@@ -27,7 +27,7 @@ function Resume() {
             <h2>Full-Stack Web Developer</h2>
           </Col>
           <Col className="">
-            <a href="../../React-portfolio/Files/JoshuaResume.pdf" download>
+            <a href="./React-portfolio/Files/JoshuaResume.pdf" download>
               <Button variant="dark" className="mt-2">
                 Download PDF
               </Button>
