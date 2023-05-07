481fa9a70d08e8914e89a2e241f36df2fc62997d
joshua-wade7 <joshua.nelson0124@gmail.com>
2023-05-07 14:29:27 -0600

    added Data directory within src directory.  Created Projects.js with Data directory to store all project data to be displayed in portfolio.

---
+++ src/Data/Projects.js
@@ -0,0 +1,25 @@
+import devloreCafe from "../images/devLore.PNG";
+import encoded from "../images/encoded.PNG";
+import digitalLibrarian from "../images/digital_librarian.PNG";
+import jate from "../images/Jate.PNG";
+import employeeTracker from "../images/employee_tracker.PNG";
+import socialNetwork from "../images/social_network_api.PNG";
+
+export const projects = [
+  {
+    image: encoded,
+    title: "Encoded",
+    description:
+      "Alternative to linkedIn with an emphasis on the tech industry and enabling the user to make deeper connections.",
+    repo: "https://github.com/therealsweven/magnificent-mustache-men",
+    id: 0,
+  },
+  {
+    image: devloreCafe,
+    title: "Devlore Cafe",
+    description:
+      "Alternative to linkedIn with an emphasis on the tech industry and enabling the user to make deeper connections.",
+    repo: "https://github.com/therealsweven/magnificent-mustache-men",
+    id: 1,
+  },
+];
