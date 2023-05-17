d28bec9b21ff4f9a0615e17ae7837d66fdb7cc0d
joshua-wade7 <joshua.nelson0124@gmail.com>
2023-05-17 17:06:26 -0600

    updating the Contact.js file with emailjs functionality.

--- src/components/Section/Contact.js
+++ src/components/Section/Contact.js
@@ -1,5 +1,5 @@
 import Form from "react-bootstrap/Form";
-import { useState, useEffect } from "react";
+import { useRef } from "react";
 import Button from "react-bootstrap/Button";
 const styles = {
   background: {
@@ -14,108 +14,115 @@ const styles = {
 };
 
 function Contact() {
-  useEffect(() => {
-    document.title = "Contact Me";
-  }, []);
+  const form = useRef();
 
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [email, setEmail] = useState("");
-  const [error, setError] = useState("");
-
-  const handleInputChange = (e) => {
-    const { target } = e;
-    const inputType = target.name;
-    const inputValue = target.value;
-
-    if (inputType === "email") {
-      setEmail(inputValue);
-    } else if (inputType === "firstName") {
-      setFirstName(inputValue);
-    } else if (inputType === "lastName") {
-      setLastName(inputValue);
-    }
-  };
-
-  const validate = (e) => {
+  const sendEmail = (e) => {
     e.preventDefault();
 
-    if (
-      !email.match(
-        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-      )
-    ) {
-      document
-        .getElementById("formEmail")
-        .setAttribute("class", "form-control border border-danger");
-      return;
-    } else if (
-      email.match(
-        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-      )
-    ) {
-      document.getElementById.setAttribute("class", "form-control");
-
-      return;
-    }
+    let validEmail = /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
   };
+  // useEffect(() => {
+  //   document.title = "Contact Me";
+  // }, []);
 
-  const formErrors = () => {
-    const { firstName, lastName, message } = Form;
-    const newErrors = {};
+  // const [firstName, setFirstName] = useState("");
+  // const [lastName, setLastName] = useState("");
+  // const [email, setEmail] = useState("");
+  // const [error, setError] = useState("");
 
-    if (!firstName || firstName === "")
-      newErrors.name = "First name cannot be blank";
-    else if (!lastName || lastName === "")
-      newErrors.name = "Last name cannot be blank";
-    else if (!message || message === "")
-      newErrors.name = "Message cannot be blank";
-    else return;
+  // const handleInputChange = (e) => {
+  //   const { target } = e;
+  //   const inputType = target.name;
+  //   const inputValue = target.value;
 
-    return newErrors;
-  };
+  //   if (inputType === "email") {
+  //     setEmail(inputValue);
+  //   } else if (inputType === "firstName") {
+  //     setFirstName(inputValue);
+  //   } else if (inputType === "lastName") {
+  //     setLastName(inputValue);
+  //   }
+  // };
 
-  const handleFormSubmit = (e) => {
-    e.preventDefault();
+  // const validate = (e) => {
+  //   e.preventDefault();
 
-    const newErrors = formErrors();
+  //   if (
+  //     !email.match(
+  //       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  //     )
+  //   ) {
+  //     document
+  //       .getElementById("formEmail")
+  //       .setAttribute("class", "form-control border border-danger");
+  //     return;
+  //   } else if (
+  //     email.match(
+  //       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  //     )
+  //   ) {
+  //     document.getElementById.setAttribute("class", "form-control");
 
-    if (Object.keys(newErrors).length > 0) {
-      setError(newErrors);
-    }
-  };
+  //     return;
+  //   }
+  // };
+
+  // const formErrors = () => {
+  //   const { firstName, lastName, message } = Form;
+  //   const newErrors = {};
+
+  //   if (!firstName || firstName === "")
+  //     newErrors.name = "First name cannot be blank";
+  //   else if (!lastName || lastName === "")
+  //     newErrors.name = "Last name cannot be blank";
+  //   else if (!message || message === "")
+  //     newErrors.name = "Message cannot be blank";
+  //   else return;
+
+  //   return newErrors;
+  // };
+
+  // const handleFormSubmit = (e) => {
+  //   e.preventDefault();
+
+  //   const newErrors = formErrors();
+
+  //   if (Object.keys(newErrors).length > 0) {
+  //     setError(newErrors);
+  //   }
+  // };
 
   return (
     <div style={styles.background}>
-      <Form className="p-5">
+      <Form className="p-5" ref={form} onSubmit={sendEmail}>
         <Form.Group className="mb-3" controlId="formFirstName">
-          <Form.Label style={styles.textColor}>First Name</Form.Label>
+          <Form.Label style={styles.textColor}>Your Name</Form.Label>
           <Form.Control
-            value={firstName}
-            onChange={handleInputChange}
+            // value={firstName}
+            // onChange={handleInputChange}
             name="firstName"
             type="text"
-            placeholder="First Name"
+            placeholder="Enter Your Name"
             style={styles.inputBody}
           />
         </Form.Group>
-        <Form.Group className="mb-3" controlId="formLastName">
+        {/* <Form.Group className="mb-3" controlId="formLastName">
           <Form.Label style={styles.textColor}>Last Name</Form.Label>
           <Form.Control
-            value={lastName}
-            onChange={handleInputChange}
+            // value={lastName}
+            // onChange={handleInputChange}
             name="lastName"
             type="text"
             placeholder="Last Name"
             style={styles.inputBody}
           />
-        </Form.Group>
+        </Form.Group> */}
         <Form.Group className="mb-3" controlId="formEmail">
           <Form.Label style={styles.textColor}>Email address</Form.Label>
           <Form.Control
-            value={email}
-            onChange={handleInputChange}
-            onClick={validate}
+            // value={email}
+            // onChange={handleInputChange}
+            // onClick={validate}
             name="email"
             type="email"
             placeholder="name@example.com"
@@ -137,7 +144,7 @@ function Contact() {
           variant="dark"
           size="lg"
           type="submit"
-          onClick={handleFormSubmit}
+          // onClick={handleFormSubmit}
         >
           Submit Here
         </Button>
